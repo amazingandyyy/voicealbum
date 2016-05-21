9cbb0aafecfd44a28e6abe8fd81d83f992c047e6
Amazingandyyy <amazingandyyy@gmail.com>
2016-05-20 20:45:37 -0700

    Initialize photolbum
    Initialize photolbum

--- public/js/module.js
+++ public/js/module.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('albumApp', ['ui.router']);
+var app = angular.module('albumApp', ['ui.router', 'ngFileUpload']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
